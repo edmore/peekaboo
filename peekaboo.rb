ceb43f484ad92146cecfc4e2abe3d4cd6cad94d4
Edmore Moyo <edmore@linux-m7qo.site>
2012-05-21 23:31:05 +0200

    Routes,views,specs and redis integration for the push page

--- peekaboo.rb
+++ peekaboo.rb
@@ -1,8 +1,33 @@
 require 'sinatra'
+require 'haml'
+require 'pry'
+require 'redis'
 
 set :haml, :format => :html5
+  
+redis = Redis.new
 
 get "/" do
-  "Peekaboo I see you!"
   haml :index
+end
+
+get "/push" do
+  status = params[:status] if params[:status]
+  unless status.nil?
+    notice =  (status == "success") ? "Successfully added." : "Oops, please enter text."
+  end
+  haml :push, :locals => {:notice => notice}
+end
+
+post "/push" do
+  title = params[:question]
+  status = ""
+  unless title  == ""
+    redis.incr "question:id"
+    question_id = redis.get("question:id")
+    redis.rpush("questions", question_id)
+    redis.set("question:#{question_id}:title", params[:question])
+    status = :success
+  end
+  redirect "/push?status=#{status}"
 end
--- spec/peekaboo_spec.rb
+++ spec/peekaboo_spec.rb
@@ -16,10 +16,38 @@ RSpec.configure do |conf|
   conf.include Rack::Test::Methods
 end
 
-feature  "Index  page" do
+feature  "Index page" do
   setup
   scenario "should display an index page" do
     visit("/")
-    page.should have_css("h1", :text => "Peekaboo")
+    page.should have_content("Peekaboo I see you")
+  end
+  
+  scenario "should link to push page" do
+    visit("/")
+    click_link("Push")
+    page.should have_content("Push")
+  end
+end
+
+feature  "Push page" do
+  setup
+  scenario "should display a push page" do
+    visit("/push")
+    page.should have_content("Push")
+  end
+
+  scenario "adding data" do
+    visit("/push")
+    fill_in('question', :with => "What's your name")
+    click_button("Push")
+    page.should have_css("p.notice", :text => "Successfully added.")
+  end
+  
+  scenario "should error if text field empty" do
+    visit("/push")
+    fill_in('question', :with => "")
+    click_button("Push")
+    page.should have_css("p.notice", :text => "Oops, please enter text.")
   end
 end
