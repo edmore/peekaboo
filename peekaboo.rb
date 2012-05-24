8f6f4760bb2a773677c8d99c68f5342682bc32c2
Edmore Moyo <etmoyo@gmail.com>
2012-05-24 09:20:26 +0200

    Mac OSX fix, use speak command if espeak not present

--- peekaboo.rb
+++ peekaboo.rb
@@ -34,7 +34,7 @@ get "/start" do
     (length).times do
       question_id = redis.lpop("questions")
       text = redis.get("question:#{question_id}:text")
-      system("espeak '#{text}'")
+      system("espeak '#{text}'") || system("speak '#{text}'")
       sleep 5
     end
     [200, {'Content-type' => 'text/plain'},"End of Session"]
