ab7e090a299f3480b331ecf53f0d126c86f4b15c
Edmore Moyo <edmore@linux-m7qo.site>
2012-05-25 21:13:50 +0200

    Adding SoundManager

---
+++ public/javascripts/start.js
@@ -0,0 +1,13 @@
+soundManager.url = 'soundmanager/swf'; 
+soundManager.debugMode = false
+
+soundManager.onready(function(){
+  if( files.length > 0 ){
+    var mySound = soundManager.createSound({
+      id: 'aSound',
+      url: "/play?filename=" + files[0][1]
+    });
+    mySound.play();
+  }
+});
+
