65021e622172931c183bcd4d2f7618d5dd3e43c3
Edmore Moyo <edmore@linux-m7qo.site>
2012-05-26 10:47:43 +0200

    Adding functionality so that sound played in a loop

--- public/javascripts/start.js
+++ public/javascripts/start.js
@@ -1,11 +1,16 @@
 soundManager.url = 'soundmanager/swf';
+soundManager.debugMode = false;
+
 soundManager.onready(function(){
   if( files.length > 0 ){
-    var mySound = soundManager.createSound({
-      id: 'aSound',
-      url: "/play?filename=" + files[0][1]
-    });
-    mySound.play();
+    for( var i=0; i < files.length; i+=1 ){
+      var mySound = soundManager.createSound({
+	id: 'Sound'+ i,
+	url: "/play?filename=" + files[i][1]
+      });
+      console.log("Currently playing...." + files[i][0]);
+      mySound.play();
+    }
   }
 });
 
